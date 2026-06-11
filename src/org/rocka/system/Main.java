9143c7f0c34749588af6e6b92f17da53c134719a
Angel Molina <amolina-2026053@kinal.edu.gt>
2026-06-11 11:45:17 -0600

    001 Se agrego clases Conexion y MenuPrincipal, se edito clase Main

--- src/org/rocka/system/Main.java
+++ src/org/rocka/system/Main.java
@@ -1,8 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package org.rocka.system;
+import org.rocka.view.MenuPrincipal;
 
 /**
  *
@@ -10,12 +7,11 @@ package org.rocka.system;
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+ 
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("hola mundo");
+       MenuPrincipal menu = new MenuPrincipal();
+       menu.iniciar();
+       
     }
     
 }
