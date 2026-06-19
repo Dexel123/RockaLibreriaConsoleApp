e80adf1a799f76521467de9cc97960c1a45c1186
Jafeth Pérez <jperez-2026078@kinal.edu.gt>
2026-06-19 11:37:01 -0600

    003 ft/cliente    se agrego la interfaz DAO para CLientes, Clientes DAO.java

---
+++ src/org/rocka/dao/ClienteDAO.java
@@ -0,0 +1,17 @@
+
+package org.rocka.dao;
+
+import java.util.List;
+import org.rocka.model.Clientes;
+
+
+public interface ClienteDAO {
+    //firmas de metodos
+    //CRUD
+    boolean insertar(Clientes cliente);
+    List<Clientes> listar();
+    Clientes buscar(long cui);
+    boolean actualizar(Clientes cliente);
+    boolean eliminar(long cui);
+   
+}
