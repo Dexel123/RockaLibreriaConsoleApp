c7b9155c9a69e7ec2a80c73db22cd8d51d1ab000
Jafeth Pérez <jperez-2026078@kinal.edu.gt>
2026-06-19 11:58:47 -0600

    004.1 feat(cliente) se agrego la imprementación para el DAO de lciente, sin completar

--- src/org/rocka/dao/impl/ClienteDAOImpl.java
+++ src/org/rocka/dao/impl/ClienteDAOImpl.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.rocka.dao.impl;
 
-/**
- *
- * @author informatica
- */
-public class ClienteDAOImpl {
+import org.rocka.util.Conexion;
+import org.rocka.dao.ClienteDAO;
+import org.rocka.model.Clientes;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.CallableStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+public class ClienteDAOImpl implements ClienteDAO{
+
+    @Override
+    public boolean insertar(Clientes cliente) {
+        return false;
+    }
+
+    @Override
+    public List<Clientes> listar() {
+        return null;
+    }
+
+    @Override
+    public Clientes buscar(long cui) {
+        return null;
+
+    }
+
+    @Override
+    public boolean actualizar(Clientes cliente) {
+        return false;
+
+    }
+
+    @Override
+    public boolean eliminar(long cui) {
+        return false;
+    }
     
 }
--- src/org/rocka/model/Clientes.java
+++ src/org/rocka/model/Clientes.java
@@ -1,13 +1,74 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.rocka.model;
 
 /**
  *
- * @author informatica
+ * @author Jafeth Pérez
  */
 public class Clientes {
+    /*
+    create table clientes(
+	cui bigint primary key,
+    nombre_cliente varchar(100),
+    apellido_cliente varchar(100),
+    correo_electronico varchar(100)
+);
+    */
+    // atributos de clase
+    long cui;
+    String nombre;
+    String apellido;
+    String correoElectronico;
+    
+    //constructores: asignacion de datos, instanciar e iniciar objetos
+    // vacio
+    public Clientes() {
+    }
+     //lleno con parametros
+    public Clientes(long cui, String nombre, String apellido, String correoElectronico) {
+        this.cui = cui;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correoElectronico = correoElectronico;
+    }
+//personalizado
+ 
+    //getter and setters
+
+    public long getCui() {
+        return cui;
+    }
+
+    public void setCui(long cui) {
+        this.cui = cui;
+    }
+
+    public String getNombre() {
+       
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        //formatear a Mayusculas
+        //formatear a Inicia con mayusculas
+        String nombreMayusculas =nombre.toUpperCase();
+        this.nombre = nombreMayusculas;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
     
 }
