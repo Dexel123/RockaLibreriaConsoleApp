5a7b6a022d4bc198349c9c7d665ca6587f43c551
Jafeth Pérez <jperez-202607@kinal.edu.gt>
2026-07-30 11:54:08 -0600

    primer commit: se agrego la base del proyecto

--- src/org/rocka/system/Main.java
+++ src/org/rocka/system/Main.java
@@ -1,17 +1,26 @@
 package org.rocka.system;
-import org.rocka.view.MenuPrincipal;
 
-/**
- *
- * @author informatica
- */
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
- 
+
+public class Main extends Application{
+
     public static void main(String[] args) {
-       MenuPrincipal menu = new MenuPrincipal();
-       menu.iniciar();
-       
+        launch(args);
     }
-    
+
+    @Override
+    public void start(Stage escenarioPrincipal) throws Exception {
+        //convertir .fxml en nodo raiz
+        Parent raiz = FXMLLoader.load(
+                getClass().getResource("/org/ac/view/InicioSesionView.fxml"));        
+        Scene escena = new Scene(raiz);
+        
+        escenarioPrincipal.setScene(escena);
+        escenarioPrincipal.show();
+    }    
 }
--- src/org/rocka/util/Conexion.java
+++ src/org/rocka/util/Conexion.java
@@ -1,18 +1,17 @@
-
 package org.rocka.util;
- 
- 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
- 
+
 public class Conexion {
     private static Conexion instancia;
+    
     // Configuración del string de conexión, y credenciales
     private static final String URL = "jdbc:mysql://localhost:3306/libreriadb_in4cm?serverTimezone=UTC";
-    private static final String USER = "admin";
-    private static final String PASSWORD = "admin";
- 
+    private static final String USER = "profesor";
+    private static final String PASSWORD = "kinal"; 
+
     //Constructor privado para evitar que hagan "new Conexion()" fuera de esta clase
     private Conexion() {
         try {
@@ -21,7 +20,7 @@ public class Conexion {
             System.err.println("Error Driver: " + e.getMessage());
         }
     }
- 
+
     //Método público estático para obtener la única instancia del Gestor
     public static synchronized Conexion getInstancia() {
         if (instancia == null) {
@@ -29,12 +28,11 @@ public class Conexion {
         }
         return instancia;
     }
- 
+
     //Método para entregar una conexión fresca cada vez que se pida
     public Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
-
+    
+    
 }
-
-
